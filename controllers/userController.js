101696514f377d452c8db365075a535aabae0d80
manjeete <manjeetkr2017@gmail.com>
2021-12-08 22:13:07 +0530

    user public profile setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
-
+const User = require("../models/userModel");
+const Blog = require("../models/blogModel");
 
 //profile
 exports.profile = (req,res) =>{
@@ -7,4 +8,42 @@ exports.profile = (req,res) =>{
         status:true,
         profile:req.profile
     })
+}
+
+
+//user public profile
+exports.publicProfile = async(req,res) =>{
+    try{
+        const username = req.params.username
+        
+        let user = await User.findOne({username})
+        if(!user){
+            return res.status(404).json({
+                status:false,
+                msg:"User not found with this username."
+            })
+        }
+
+        const blogs = await Blog.find({postedBy:user._id})
+                        .populate('categories','_id name slug')
+                        .populate('tags','_id name slug')
+                        .populate('postedBy','_id name')
+                        .limit(10)
+                        .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+
+        user.photo = undefined
+        user.hash_password=undefined
+        res.status(200).json({
+            status:true,
+            user,
+            blogs
+        })
+
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            status:false,
+            msg:err.message 
+        })
+    }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
 
 router.get('/profile',authController.requireSignin,authController.authMiddleware,userController.profile);
+router.get('/:username',userController.publicProfile)
 
 module.exports = router;
